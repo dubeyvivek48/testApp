716d77cc995766daa7d7bfa852265cb61fe68c0d
vivek <duebyvivek48@gmail.com>
2020-05-15 20:53:13 +0100

    Updates

---
+++ precache-manifest.020dbfde384342b49cc22c0e548eb37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35b626b53546d9ef414a4c07348ae2c",
+    "url": "/dubeyvivek48.github.io/assignment/index.html"
+  },
+  {
+    "revision": "80ab3defb20b70f10856",
+    "url": "/dubeyvivek48.github.io/assignment/static/css/main.b6793e35.chunk.css"
+  },
+  {
+    "revision": "247c012fdbe631986e8c",
+    "url": "/dubeyvivek48.github.io/assignment/static/js/2.e03eea73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dubeyvivek48.github.io/assignment/static/js/2.e03eea73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ab3defb20b70f10856",
+    "url": "/dubeyvivek48.github.io/assignment/static/js/main.d59e4780.chunk.js"
+  },
+  {
+    "revision": "0aaf780a3f57eb7916e3",
+    "url": "/dubeyvivek48.github.io/assignment/static/js/runtime-main.c20e9787.js"
+  }
+]);
