f9c8659051f8ad401345fa158c0c4f4e92453e6b
vivek <duebyvivek48@gmail.com>
2020-05-15 20:39:55 +0100

    add bulid for github pages

---
+++ build/precache-manifest.9c5fcff6add2a5ab0f50969e20a98150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e910f4920411a0d247c946bf03c3cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b348bcb25d86dceccc2",
+    "url": "/static/css/main.b6793e35.chunk.css"
+  },
+  {
+    "revision": "247c012fdbe631986e8c",
+    "url": "/static/js/2.e03eea73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e03eea73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b348bcb25d86dceccc2",
+    "url": "/static/js/main.5a30ce6c.chunk.js"
+  },
+  {
+    "revision": "c071e0c5c28957d8085a",
+    "url": "/static/js/runtime-main.4500f0e6.js"
+  }
+]);
